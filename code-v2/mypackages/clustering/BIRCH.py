bbc9d58ad433882889b75635da20ace831d354e5
EDLMM <37733136+EDLMM@users.noreply.github.com>
2018-10-25 21:06:03 +0800

    Update BIRCH.py

--- code-v2/mypackages/clustering/BIRCH.py
+++ code-v2/mypackages/clustering/BIRCH.py
@@ -1,1 +1,11 @@
-#
+#just try a hierarchical
+
+from sklearn.cluster import Birch
+
+def getCluster(org_data,branching_factor=50,n_clusters=4,threshold=0.5,compute_labels=True):
+	brc = Birch(branching_factor=50, n_clusters=None, threshold=0.5,
+	compute_labels=True)
+	brc.fit(org_data)
+	d_labels=brc.predict(X)
+	
+	return d_labels
